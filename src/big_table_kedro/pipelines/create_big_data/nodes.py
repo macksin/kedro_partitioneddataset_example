5e1bc7860e40143072188e44b4884d638bcbac23
mashcon <mack.heyha@gmail.com>
2022-06-12 09:14:16 -0300

    Add PartitionedDataSet style output function.

--- src/big_table_kedro/pipelines/create_big_data/nodes.py
+++ src/big_table_kedro/pipelines/create_big_data/nodes.py
@@ -2,14 +2,32 @@
 This is a boilerplate pipeline 'create_big_data'
 generated using Kedro 0.18.1
 """
-from random import random
+
 import numpy as np
 from sklearn.datasets import (make_classification)
 import pandas as pd
+import logging
+from typing import Any, Dict, Callable
 
-def make_dataset(random_seed: int = 42) -> pd.DataFrame:
-    X, y = make_classification(n_samples=1000, n_features=20, random_state=random_seed)
+logger = logging.getLogger(__name__)
+
+
+def make_dataset(random_seed: int = 42, sample_size: int = 10) -> pd.DataFrame:
+    X, y = make_classification(n_samples=sample_size, n_features=20,
+                               random_state=random_seed)
     data = np.concatenate((X, y.reshape((-1, 1))), axis=1)
     df = pd.DataFrame(data, columns=[f"col_{i}" for i in range(data.shape[1])])
     df["col_20"] = df["col_20"].astype(int)
     return df
+
+
+def generate_big_data(
+    random_seed: int = 42,
+    creation_size: int = 10
+) -> Dict[str, Callable[[], Any]]:
+
+    rng = np.random.seed(random_seed)
+
+    return {
+        f"dataset_part_{i}": lambda: make_dataset(random_seed=rng)
+        for i in range(creation_size)}
